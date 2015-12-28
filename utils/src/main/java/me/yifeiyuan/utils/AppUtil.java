6ed46c7abe1e00caeb78fb863cd9e1647a59bd4a
AlanCheen <2985697630@qq.com>
2015-12-28 17:51:07 +0800

    add apputil

---
+++ utils/src/main/java/me/yifeiyuan/utils/AppUtil.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2015, 程序亦非猿
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.yifeiyuan.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by 程序亦非猿 on 15/12/28.
+ */
+public class AppUtil {
+
+    /**
+     * @param context 上下文
+     * @return app的版本名
+     */
+    public static String getVersionName(Context context)
+    {
+        PackageManager pm = context.getPackageManager();
+        try
+        {
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     *
+     * @param context 上下文
+     * @return 获取版本号
+     */
+    public static int getVersionCode(Context context)
+    {
+        PackageManager pm = context.getPackageManager();
+        try
+        {
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
