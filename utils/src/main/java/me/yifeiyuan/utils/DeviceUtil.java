bfabd2e713df6d304f305be87c3d2f64548ffb80
AlanCheen <2985697630@qq.com>
2015-12-29 15:28:46 +0800

    update deviceutil

--- utils/src/main/java/me/yifeiyuan/utils/DeviceUtil.java
+++ utils/src/main/java/me/yifeiyuan/utils/DeviceUtil.java
@@ -16,8 +16,87 @@
 
 package me.yifeiyuan.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.telephony.TelephonyManager;
+
 /**
  * Created by 程序亦非猿 on 15/12/26.
+ *
+ * 包含如下方法
+ * 1.getDeviceId            获取IMEI
+ * 2.isNetworkConnected     网络是否连接
+ * 3.isWifiConnected        wifi是否可用
+ * 4.getCurNetType          当前网络
  */
 public class DeviceUtil {
+
+    /**
+     * 获取设备唯一标识
+     * 例子:IMEI for GSM and the MEID or ESN for CDMA phones
+     * 注意,需要添加权限: {@link android.Manifest.permission#READ_PHONE_STATE}
+     *
+     * @param context 上下文
+     * @return eg.IMEI for GSM
+     */
+    public static String getDeviceId(@NonNull Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm.getDeviceId();
+    }
+
+    /**
+     * 网络是否连接
+     *
+     * @param context 上下文
+     * @return true 如果网络连接着
+     */
+    public static boolean isNetworkConnected(Context context) {
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        return null != info && info.isAvailable();
+    }
+
+    /**
+     * wifi是否连接可用
+     *
+     * @param context
+     * @return true 如果wifi可用
+     */
+    public static boolean isWifiConnected(Context context) {
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return info != null && info.isConnected();
+    }
+
+    /**
+     * 得到当前的手机网络类型
+     *
+     * @param context
+     * @return 没网返回"" , 有网返回 2g / 3g /4g/wifi
+     */
+    public static String getCurNetType(Context context) {
+        String type = "";
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info == null) {
+            type = "";
+        } else if (info.getType() == ConnectivityManager.TYPE_WIFI) {
+            type = "wifi";
+        } else if (info.getType() == ConnectivityManager.TYPE_MOBILE) {
+            int subType = info.getSubtype();
+            if (subType == TelephonyManager.NETWORK_TYPE_CDMA || subType == TelephonyManager.NETWORK_TYPE_GPRS
+                    || subType == TelephonyManager.NETWORK_TYPE_EDGE) {
+                type = "2g";
+            } else if (subType == TelephonyManager.NETWORK_TYPE_UMTS || subType == TelephonyManager.NETWORK_TYPE_HSDPA
+                    || subType == TelephonyManager.NETWORK_TYPE_EVDO_A || subType == TelephonyManager.NETWORK_TYPE_EVDO_0
+                    || subType == TelephonyManager.NETWORK_TYPE_EVDO_B) {
+                type = "3g";
+            } else if (subType == TelephonyManager.NETWORK_TYPE_LTE) {// LTE是3g到4g的过渡，是3.9G的全球标准
+                type = "4g";
+            }
+        }
+        return type;
+    }
 }
