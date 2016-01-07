9c4a8387e3a0093f96bc00606b61f39345aa6099
AlanCheen <2985697630@qq.com>
2016-01-07 18:57:43 +0800

    add methods

--- utils/src/main/java/me/yifeiyuan/utils/ScreenUtil.java
+++ utils/src/main/java/me/yifeiyuan/utils/ScreenUtil.java
@@ -18,6 +18,7 @@ package me.yifeiyuan.utils;
 
 import android.content.Context;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.WindowManager;
 
 /**
@@ -49,5 +50,37 @@ public class ScreenUtil {
         DisplayMetrics outMetrics = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(outMetrics);
         return outMetrics.heightPixels;
+    }
+
+    /**
+     * 获取状态栏高度
+     * @param context
+     * @return
+     */
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+
+        return result;
+    }
+
+    /**
+     * 获取toolbar的高度
+     * @param context
+     * @return
+     */
+    public static int getToolbarHeight(Context context) {
+        TypedValue typedValue = new TypedValue();
+
+        if (context.getTheme().resolveAttribute(android.R.attr.actionBarSize, typedValue, true)) {
+            return TypedValue.complexToDimensionPixelSize(typedValue.data, context.getResources().getDisplayMetrics());
+        }
+
+        return 0;
     }
 }
