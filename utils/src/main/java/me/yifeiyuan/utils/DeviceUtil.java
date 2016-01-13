b1a1680ec2b3ce57f71b72ef19666230ee59d12b
AlanCheen <2985697630@qq.com>
2016-01-13 10:21:02 +0800

    新增获取cou数量的方法

--- utils/src/main/java/me/yifeiyuan/utils/DeviceUtil.java
+++ utils/src/main/java/me/yifeiyuan/utils/DeviceUtil.java
@@ -19,8 +19,12 @@ package me.yifeiyuan.utils;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.telephony.TelephonyManager;
+
+import java.io.File;
+import java.io.FileFilter;
 
 /**
  * Created by 程序亦非猿 on 15/12/26.
@@ -32,6 +36,12 @@ import android.telephony.TelephonyManager;
  * 4.getCurNetType          当前网络
  */
 public class DeviceUtil {
+
+    /**
+     * 默认单核
+     */
+    public static final int DEFAULT_CPU_COUNT = 1;
+
 
     /**
      * 获取设备唯一标识
@@ -103,4 +113,49 @@ public class DeviceUtil {
         }
         return type;
     }
+
+
+
+
+    /**
+     *
+     * @By 小邓子
+     * Linux中的设备都是以文件的形式存在，CPU也不例外，因此CPU的文件个数就等价与核数。
+     * Android的CPU 设备文件位于/sys/devices/system/cpu/目录，文件名的的格式为cpu\d+。
+     *
+     * 引用：http://www.jianshu.com/p/f7add443cd32#，感谢 liangfeizc :)
+     * https://github.com/facebook/device-year-class
+     */
+    /**
+     *
+     * @return CPU的个数 默认为1
+     */
+    public static int getCountOfCPU() {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
+            //不支持
+            return DEFAULT_CPU_COUNT;
+        }
+        int count;
+        try {
+            count = new File("/sys/devices/system/cpu/").listFiles(CPU_FILTER).length;
+        } catch (SecurityException | NullPointerException e) {
+            count = DEFAULT_CPU_COUNT;
+        }
+        return count;
+    }
+
+    private static final FileFilter CPU_FILTER = new FileFilter() {
+        @Override public boolean accept(File pathname) {
+            String path = pathname.getName();
+            if (path.startsWith("cpu")) {
+                for (int i = 3; i < path.length(); i++) {
+                    if (path.charAt(i) < '0' || path.charAt(i) > '9') {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    };
 }
