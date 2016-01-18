b733aa8792ab4b34f70f7c646de416e1ee328581
AlanCheen <2985697630@qq.com>
2016-01-18 12:02:01 +0800

    add patternutil

---
+++ utils/src/main/java/me/yifeiyuan/utils/PatternUtil.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2015, 程序亦非猿
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.yifeiyuan.utils;
+
+import android.support.annotation.NonNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by 程序亦非猿 on 16/1/18.
+ */
+public class PatternUtil {
+
+
+    /**
+     * 判断是否是手机号码 13 14 15 17 18 开头
+     * @param phone
+     * @return 是否是手机号
+     */
+    public static boolean isMobile(@NonNull String phone) {
+        Pattern p = Pattern.compile("^[1][3,4,5,7,8][0-9]{9}$");
+        Matcher m = p.matcher(phone);
+        return m.matches();
+    }
+
+}
